e312370325ed03816e6df423619533356a39823d
anees715 <anees715@gmail.com>
2018-06-03 18:57:39 +0530

    add successful request action

---
+++ src/actions/successful-request-action.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+let mock = new MockAdapter(axios);
+mock.onGet('/successful-request').reply(200, {
+  serverMessage: "Your action was successfull!"
+});
+
+export default successfulRequest = () => {
+    return async (dispatch) => {
+        const response = await axios.get('/successful-request');
+    }
+}
